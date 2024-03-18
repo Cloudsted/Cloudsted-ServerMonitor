418252dfff07f678058c0f4fd162c2a4ce3998e2
Brian Baker <brian.baker@fundingshield.com>
2024-03-18 14:55:41 -0700

    First Release

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const cloudsted = require('cloudsted');
+require('dotenv').config()
 
 const cloudstedAPIKey = process.env.CLOUDSTED_API_KEY;
-cloudsted.init();
+const serverName = process.env.SERVER_NAME || 'My Server';
 
 const oneMinuteMs = 60 * 1000;
 
 const logIntervalMs = process.env.LOG_INTERVAL_MS || oneMinuteMs;
+
+// Log Metrics
+beginLoggingMetrics();
+
+async function beginLoggingMetrics() {
+
+    await cloudsted.initAPIKey(cloudstedAPIKey);
+
+    logMetrics();
+
+    // Log metrics every logIntervalMs
+    setInterval(async () => {
+        logMetrics();
+    }, logIntervalMs);
+}
+
+async function logMetrics() {
+    cloudsted.logServerMetrics(serverName);
+    console.log('Logged server metrics!')
+}
